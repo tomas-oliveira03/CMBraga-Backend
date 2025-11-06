34af1829d55d3994181744f07f47fdf1972eee35
tomas-oliveira03 <tumabarbosa@gmail.com>
2025-11-06 14:33:20 +0000

    refactor: default profile pictures

--- src/helpers/storage.ts
+++ src/helpers/storage.ts
@@ -1,11 +1,23 @@
 export const MAX_KML_SIZE = 1 * 1024 * 1024 // 1MB
 
 export const USER_DEFAULT_PROFILE_PICTURES = [
-    "https://res.cloudinary.com/dwffdkytm/image/upload/v1760867592/CMBraga/users/default-profile-picture1.jpg",
-    "https://res.cloudinary.com/dwffdkytm/image/upload/v1760867592/CMBraga/users/default-profile-picture2.jpg"
+    "https://res.cloudinary.com/dwffdkytm/image/upload/v1762439325/CMBraga/users/default-profile-picture-1.jpg",
+    "https://res.cloudinary.com/dwffdkytm/image/upload/v1762439325/CMBraga/users/default-profile-picture-2.jpg",
+    "https://res.cloudinary.com/dwffdkytm/image/upload/v1762439325/CMBraga/users/default-profile-picture-3.jpg",
+    "https://res.cloudinary.com/dwffdkytm/image/upload/v1762439325/CMBraga/users/default-profile-picture-4.jpg",
+    "https://res.cloudinary.com/dwffdkytm/image/upload/v1762439325/CMBraga/users/default-profile-picture-5.jpg",
+    "https://res.cloudinary.com/dwffdkytm/image/upload/v1762439325/CMBraga/users/default-profile-picture-6.jpg",
+    "https://res.cloudinary.com/dwffdkytm/image/upload/v1762439325/CMBraga/users/default-profile-picture-7.jpg",
+    "https://res.cloudinary.com/dwffdkytm/image/upload/v1762439325/CMBraga/users/default-profile-picture-8.jpg",
+    "https://res.cloudinary.com/dwffdkytm/image/upload/v1762439325/CMBraga/users/default-profile-picture-9.jpg",
+    "https://res.cloudinary.com/dwffdkytm/image/upload/v1762439325/CMBraga/users/default-profile-picture-10.jpg",
+    "https://res.cloudinary.com/dwffdkytm/image/upload/v1762439325/CMBraga/users/default-profile-picture-11.jpg",
+    "https://res.cloudinary.com/dwffdkytm/image/upload/v1762439325/CMBraga/users/default-profile-picture-12.jpg",
+    "https://res.cloudinary.com/dwffdkytm/image/upload/v1762439325/CMBraga/users/default-profile-picture-13.jpg",
+    "https://res.cloudinary.com/dwffdkytm/image/upload/v1762439325/CMBraga/users/default-profile-picture-14.jpg"
 ]
 
-export const GROUP_DEFAULT_PROFILE_PICTURE = "https://res.cloudinary.com/dwffdkytm/image/upload/v1762359163/CMBraga/group/default-profile-picture-1.jpg"
+export const GROUP_DEFAULT_PROFILE_PICTURE = "https://res.cloudinary.com/dwffdkytm/image/upload/v1762439258/CMBraga/groups/default-group-picture-1.jpg"
 
 const allowedImageTypes = ["image/jpeg", "image/jpg", "image/png", "image/webp"]
 
