27c10075c55a7610bdd4deeb0d6f2cac5adf2660
tomas-oliveira03 <tumabarbosa@gmail.com>
2025-11-19 14:51:31 +0000

    feat(server): add caddy as reverse proxy

--- src/server/routers/health.ts
+++ src/server/routers/health.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from "express";
 const router = express.Router();
 
 router.get("/", (req: Request, res: Response) => {
-    res.status(200).json({
+    return res.status(200).json({
         status: "OK",
         timestamp: new Date().toISOString(),
         uptime: process.uptime(),
